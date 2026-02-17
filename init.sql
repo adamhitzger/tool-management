982d98269216a548463e7eee0253207d6a2e4bf6
adamhitzger <adam.hitzger@icloud.com>
2026-02-17 08:50:14 +0100

    init.sql

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS organizations (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     slug TEXT UNIQUE NOT NULL,
-    created_at INT NOT NULL,
+    created_at BIGINT NOT NULL,
     icon_name TEXT
 );
 
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS tools (
     id VARCHAR(14) PRIMARY KEY,
     type TEXT NOT NULL,
     is_ok FLOAT NOT NULL,
-    first_usage INT NOT NULL,
+    first_usage BIGINT NOT NULL,
     organization_id INT NOT NULL REFERENCES organizations(id) ON DELETE CASCADE,
     machine_id INT REFERENCES machines(id) ON DELETE SET NULL
 );
@@ -29,8 +29,8 @@ CREATE TABLE IF NOT EXISTS tools (
 CREATE TABLE IF NOT EXISTS tools_history (
     id SERIAL PRIMARY KEY,
     tool_id VARCHAR(14) NOT NULL REFERENCES tools(id) ON DELETE CASCADE,
-    start INT NOT NULL,
-    "end" INT,
+    start BIGINT NOT NULL,
+    "end" BIGINT,
     obrobek_id TEXT NOT NULL,
     korekce TEXT NOT NULL,
     organization_id INT NOT NULL REFERENCES organizations(id) ON DELETE CASCADE
@@ -58,8 +58,16 @@ CREATE TABLE IF NOT EXISTS verification_codes (
     id SERIAL PRIMARY KEY,
     identifier TEXT NOT NULL,
     code INT NOT NULL CHECK (code >= 0 AND code <= 999999),
-    created_at INT NOT NULL,
-    expires_at INT NOT NULL,
-    used_at INT,
+    created_at BIGINT NOT NULL,
+    expires_at BIGINT NOT NULL,
+    used_at BIGINT,
     UNIQUE (identifier, code)
 );
+
+INSERT INTO organizations (name, slug, created_at, icon_name)
+VALUES
+('Teijin', 'teijin', 1768466310, 'teijin.png'),
+('Houfek', 'houfek', 1769696646155, 'houfek.png');
+
+INSERT INTO users (email, name, surname, role, organization_id)
+VALUES ('adam.hitzger@icloud.com', 'Adam', 'Hitzger', 'SUPER_ADMIN', 1);
